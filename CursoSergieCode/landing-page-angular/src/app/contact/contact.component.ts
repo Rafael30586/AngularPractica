4430eb5465d15de6a2d8741bf0415b9bffeb2d0e
Rafael305 <fraq86@gmail.com>
2025-07-24 16:25:21 -0300

    formulario reactivo

--- CursoSergieCode/landing-page-angular/src/app/contact/contact.component.ts
+++ CursoSergieCode/landing-page-angular/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  formularioContacto: FormGroup
+
+  constructor(private form: FormBuilder){
+
+    this.formularioContacto = this.form.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]]
+    })
+
+  }
+
+  enviar(){
+    console.log(this.formularioContacto)
+  }
 
 }
