0d0566cf37fdb17c1578ea5226d3cc1a460416f9
Rafael305 <fraq86@gmail.com>
2025-07-31 16:37:23 -0300

    api service

--- CursoSergieCode/landing-page-angular/src/app/services/api.service.ts
+++ CursoSergieCode/landing-page-angular/src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IProduct } from '../models/product.model';
+import { Category, IProduct } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,28 @@ export class ApiService {
 
   constructor(private _httpClient: HttpClient) { }
 
-  public getProducts(): Observable<IProduct[]>{
-    return this._httpClient.get<IProduct[]>(this.baseUrl)
+  public getAllProducts(): Observable<IProduct[]>{ // Devuelve un observable de productos
+    
+    return this._httpClient.get<IProduct[]>(`${this.baseUrl}`)
   }
 
-  public getProductById(id: number): Observable<IProduct>{
+  public getProductById(id: number): Observable<IProduct>{ // Devuelve un observable de un solo producto
     return this._httpClient.get<IProduct>(`${this.baseUrl}/${id}`)
+  }
+
+  public getAllCategories(): Observable<Category[]>{ // Devuelve un observable de todasd las categorías
+    return this._httpClient.get<Category[]>(`${this.baseUrl}/categories`)
+  }
+
+  public newProduct(product: IProduct): Observable<IProduct>{ // Devuelve un observable de un producto
+    return this._httpClient.post<IProduct>(`${this.baseUrl}`, product)
+  }
+
+  public updateProduct(id: number, product: IProduct): Observable<IProduct>{
+    return this._httpClient.put<IProduct>(`${this.baseUrl}/${id}`,product)
+  }
+
+  public deleteProduct(id: number): Observable<IProduct>{
+    return this._httpClient.delete<IProduct>(`${this.baseUrl}/${id}`)
   }
 }
