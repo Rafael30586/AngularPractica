21810e21416347ef9af3daad579c06666442881a
Rafael305 <fraq86@gmail.com>
2025-08-01 17:09:02 -0300

    bootstrap card con productos

--- CursoSergieCode/landing-page-angular/src/app/product-details/product-details.component.ts
+++ CursoSergieCode/landing-page-angular/src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Product, productsList } from '../products/products.mock';
+import { IProduct } from '../models/product.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-product-details',
@@ -9,23 +11,33 @@ import { Product, productsList } from '../products/products.mock';
 })
 export class ProductDetailsComponent implements OnInit{
 
-  product?: Product
-  productList: Product[] = productsList
+  product?: IProduct
+  productList: IProduct[] = []
   loading: boolean = true
   color: string = ''
 
-  constructor(private _route: ActivatedRoute){
-    
-
-  }
+  constructor(private _route: ActivatedRoute,
+              private _apiService: ApiService
+  ){}
 
   ngOnInit(): void {
-    setTimeout(()=>{
-      this._route.params.subscribe(params => {
-      this.product = this.productList.find(product => product.id == params['productId'])
-      this.color = this.product?.price as number > 5 ? 'red' : ''
-      this.loading = false
+    this._route.params.subscribe({
+      next: (params: Params) => {
+        this._apiService.getProductById(Number(params['productId'])).subscribe({
+          next: (data: IProduct) => {
+            this.product = data
+            this.color = this.product?.price as number > 5 ? 'red' : ''
+            this.loading = false
+            error: (error: any) => {
+              console.log(error)
+              this.loading = false
+            }
+          }
+        })
+      },
+      error: (error: any) => {
+        console.log(error)
+      }
     })
-    },1500)
   }
 }
