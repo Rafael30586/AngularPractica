0eae2a16e67cf20403837738bd74c08b49db0bbb
Rafael305 <fraq86@gmail.com>
2025-08-11 16:41:15 -0300

    tarjeta para productos

--- Angular17SergieCode/landingPageAngular17/src/app/pages/product-details/product-details.component.ts
+++ Angular17SergieCode/landingPageAngular17/src/app/pages/product-details/product-details.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
+
+  loading: boolean = true
+  public product?: IProduct
+
+  private _route = inject(ActivatedRoute)
+  private _apiService = inject(ApiService)
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params=>{
+      this._apiService.getProduct(params['id']).subscribe((data:IProduct)=>{
+        this.product = data
+        this.loading = false
+      })
+    })
+  }
 
 }
--- Angular17SergieCode/landingPageAngular17/src/app/pages/product/product.component.ts
+++ Angular17SergieCode/landingPageAngular17/src/app/pages/product/product.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { IProduct } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -14,11 +16,16 @@ export class ProductComponent implements OnInit{
   productList: IProduct[] = []
 
   private _apiService = inject(ApiService)
+  private _router = inject(Router)
 
   ngOnInit(): void {
     this._apiService.getProducts().subscribe((data: IProduct[])=>{
       this.productList = data
     })
+  }
+
+  navigate(id:number){
+    this._router.navigate(['/products',id])
   }
 
 
