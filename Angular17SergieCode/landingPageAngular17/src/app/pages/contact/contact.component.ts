b6f5d499ca41f879a915a1dec7be63282f8c89ed
Rafael305 <fraq86@gmail.com>
2025-08-12 16:32:54 -0300

    uso de formBuilder

--- Angular17SergieCode/landingPageAngular17/src/app/pages/contact/contact.component.ts
+++ Angular17SergieCode/landingPageAngular17/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  
+  contactForm!: FormGroup  
+
+  constructor(private formBuilder: FormBuilder){
+    this.contactForm = this.formBuilder.group({
+      email: ['',[Validators.required,Validators.email]],
+      message: ['',[Validators.required, Validators.minLength(10)]]
+    })
+  }
+
+  enviar(event: Event){
+    event.preventDefault()
+    console.log(this.contactForm.value)
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
