5bb4162fd92cd9f718b3d6ec73a852198583f267
Rafael305 <fraq86@gmail.com>
2025-09-03 16:34:28 -0300

    practica con ngclass

--- Practica/directivas/src/app/componente/componente.component.ts
+++ Practica/directivas/src/app/componente/componente.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { UnServicioService } from '../servicios/un-servicio.service';
 
 @Component({
@@ -6,14 +6,25 @@ import { UnServicioService } from '../servicios/un-servicio.service';
   templateUrl: './componente.component.html',
   styleUrls: ['./componente.component.css']
 })
-export class ComponenteComponent {
+export class ComponenteComponent implements DoCheck{
   color = '#066727'
   color2 = '#c645d9'
 
+  pinkParty?: boolean 
+
   constructor(private _unServicioService: UnServicioService){
   }
+  
 
   public cambiarFoto(){
     this._unServicioService.cambiarFoto()
+  }
+
+  public cambiarColor(){
+    this._unServicioService.changeColor()
+  }
+
+  ngDoCheck(): void {
+    this.pinkParty = this._unServicioService.getColor()
   }
 }
--- Practica/directivas/src/app/servicios/un-servicio.service.ts
+++ Practica/directivas/src/app/servicios/un-servicio.service.ts
@@ -6,6 +6,7 @@ import { Injectable, OnInit } from '@angular/core';
 export class UnServicioService implements OnInit{
 
   numeroFoto: number = 1
+  pinkParty: boolean = false
 
   constructor() { }
   
@@ -23,11 +24,23 @@ export class UnServicioService implements OnInit{
       case 2: this.setNumeroFoto(3);break;
       case 3: this.setNumeroFoto(1);break;
     }
-    console.log('Cambiando a numero: ',this.numeroFoto)
+    //console.log('Cambiando a numero: ',this.numeroFoto)
+  }
+
+  public changeColor(){
+    if(!this.pinkParty){
+      this.pinkParty = true
+    }else{
+      this.pinkParty = false
+    }
+  }
+
+  public getColor(){
+    return this.pinkParty
   }
 
   ngOnInit(): void {
     // this.setNumeroFoto(1)
-    console.log('Numero foto desde el servicio:',this.numeroFoto)
+    //console.log('Numero foto desde el servicio:',this.numeroFoto)
   }
 }
